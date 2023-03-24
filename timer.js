066fe45969fe6c39d90c92a0df3e2979385a634d
a-me01 <104769384+a-me01@users.noreply.github.com>
2023-03-24 18:45:09 +0100

    HTML and JS

---
+++ timer.js
@@ -0,0 +1,76 @@
+// clear button to clear the li list
+const clearButton = document.getElementById("clear-button");
+const ulList = document.getElementById("list");
+clearButton.addEventListener("click", function () {
+    ulList.innerHTML = "";
+});
+
+
+// form to generate li into the ul
+function generateList(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const seconds = document.getElementById("seconds").value;
+    const li = document.createElement("li");
+    li.innerHTML = `${name} ${seconds}s`;
+    const ul = document.getElementById("list");
+    ul.appendChild(li);
+}
+
+// interval timer function
+function startTimer() {
+
+// storing li's into variable
+    const lis = document.querySelectorAll("#list li");
+
+// remove "current" class from all li on execution
+    for (let i = 0; i < lis.length; i++) {
+        lis[i].classList.remove("current");
+    }
+
+// variable setting
+    let currentIndex = 0;
+    let remainingSeconds = 0;
+    let intervalId;
+
+// updated, timer ends in 0
+    function countdown() {
+        if (remainingSeconds === 0) {
+            if (currentIndex >= lis.length) {
+                clearInterval(intervalId);
+                remainingSeconds = 0;
+                const timerElement = document.getElementById("timer");
+                timerElement.innerHTML = "0:00";
+                return;
+            }
+// Remove the "current" class from the previous li element
+            if (currentIndex > 0) {
+                lis[currentIndex - 1].classList.remove("current");
+            }
+            const li = lis[currentIndex];
+// destructured array - makes 2 variables in one line using split() method
+            const [name, seconds] = li.innerHTML.split(" ");
+            remainingSeconds = parseInt(seconds, 10);
+            currentIndex++;
+// Add the "current" class to the current li element
+            li.classList.add("current");
+        }
+//calculates the number of full minutes remaining, rounded down to the nearest integer.
+        const minutes = Math.floor(remainingSeconds / 60);
+// calculates the number of seconds remaining after subtracting the full minutes.
+        const seconds = remainingSeconds % 60;
+// padstart method to format the seconds in the countdown,
+// first argument is how long string should be, second argument is 
+// the number to use for padding (ie. 01:03, rather than 1:3)
+        const timeString = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+        const timerElement = document.getElementById("timer");
+        timerElement.innerHTML = timeString;
+        remainingSeconds--;
+    }
+    countdown();
+
+    intervalId = setInterval(countdown, 1000);
+}
+
+
+
